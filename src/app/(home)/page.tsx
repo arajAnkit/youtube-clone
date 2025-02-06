b2d9ae3fd95a6208eb97a8b9bafc30da5caa6396
Ankit Rajpoot <marketing.araj@gmail.com>
2025-02-06 21:33:07 +0530

    pt2 - Home sidebar layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="">
-      <Image src="/logo.svg" height={50} width={50} alt="logo" />
-      <p className="text-xl font-semibold tracking-tight">NewTube</p>
+      I will load videos in the future!
     </div>
   );
 }
