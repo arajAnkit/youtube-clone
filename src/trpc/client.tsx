b31cb2a145e9877846a7ac9aab0c8905093baed8
Ankit Rajpoot <marketing.araj@gmail.com>
2025-02-13 10:21:34 +0530

    pt7 - TRPC configuration

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,17 +1,20 @@
-'use client';
+"use client";
 // ^-- to make sure we can mount the Provider from a server component
-import type { QueryClient } from '@tanstack/react-query';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { httpBatchLink } from '@trpc/client';
-import { createTRPCReact } from '@trpc/react-query';
-import { useState } from 'react';
-import { makeQueryClient } from './query-client';
-import type { AppRouter } from './routers/_app';
+import type { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
+
+import { useState } from "react";
+import superjson from "superjson";
+
+import { makeQueryClient } from "./query-client";
+import type { AppRouter } from "./routers/_app";
 
 export const trpc = createTRPCReact<AppRouter>();
 let clientQueryClientSingleton: QueryClient;
 function getQueryClient() {
-  if (typeof window === 'undefined') {
+  if (typeof window === "undefined") {
     // Server: always make a new query client
     return makeQueryClient();
   }
@@ -20,16 +23,16 @@ function getQueryClient() {
 }
 function getUrl() {
   const base = (() => {
-    if (typeof window !== 'undefined') return '';
+    if (typeof window !== "undefined") return "";
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-    return 'http://localhost:3000';
+    return "http://localhost:3000";
   })();
   return `${base}/api/trpc`;
 }
 export function TRPCProvider(
   props: Readonly<{
     children: React.ReactNode;
-  }>,
+  }>
 ) {
   // NOTE: Avoid useState when initializing the query client if you don't
   //       have a suspense boundary between this and the code that may
@@ -40,11 +43,16 @@ export function TRPCProvider(
     trpc.createClient({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson, //<-- if you use a data transformer
           url: getUrl(),
+          async headers() {
+            const headers = new Headers();
+            headers.set("x-trpc-souce", "nextjs-react");
+            return headers;
+          },
         }),
       ],
-    }),
+    })
   );
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
