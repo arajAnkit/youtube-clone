a0c31daf2748719695310d45cef85318be750066
Ankit Rajpoot <marketing.araj@gmail.com>
2025-03-07 11:15:10 +0530

    part16.1 - Background job setup

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -10,7 +10,23 @@ import { db } from "@/db";
 import { mux } from "@/lib/mux";
 import { videos, videoUpdateSchema } from "@/db/schema";
 
+import { workflow } from "@/lib/workflow";
+
 export const videosRouter = createTRPCRouter({
+  generateThumbnail: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
+      const { workflowRunId } = await workflow.trigger({
+        url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/title`,
+        body: { userId, videoId: input.id },
+      });
+      return workflowRunId;
+    }),
   restoreThumbnail: protectedProcedure
     .input(
       z.object({
