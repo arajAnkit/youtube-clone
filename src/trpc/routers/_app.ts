d33f844df2aec4b56fbce7e565564ac4c570da4d
Ankit Rajpoot <marketing.araj@gmail.com>
2025-03-31 08:24:04 +0530

    pt20 (4.Modified):Video tow row and Video reactions components

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,12 +4,14 @@ import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
+import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   studio: studioRouter,
   videos: videosRouter,
   videoViews: videoViewsRouter,
+  videoReactions: videoReactionsRouter,
 });
 
 export type AppRouter = typeof appRouter;
